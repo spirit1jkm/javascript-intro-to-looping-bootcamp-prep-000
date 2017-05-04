62f2c25cff159090e94ce39f8cefe89ecce94da9
Jennifer Watson <jennifer.watson@me.com>
2017-05-04 18:36:36 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
   for (i = 0, l = array.length; i < l; i++) {
-    if (i === 1) {
-      array = [...array, "I am 1 strange loop."];
+    if (i == 1) {
+      array.push("I am 1 strange loop.");
       return array;
     } else {
-      array = [...array, `I am ${i} strange loops.`];
+      array.push(`I am ${i} strange loops.`);
       return array;
     }
   }
