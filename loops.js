011a6b204f2d21291f12312eddd07b7343747e79
Jennifer Watson <jennifer.watson@me.com>
2017-05-04 19:27:42 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,10 +1,12 @@
 function forLoop(array) {
   for (i = 0, l = array.length; i < l; i++) {
     if (i == 1) {
-      array.push("I am 1 strange loop.");
+      array = [...array, "I am 1 strange loop."];
+      //array.push("I am 1 strange loop.");
       return array;
     } else {
-      array.push(`I am ${i} strange loops.`);
+      array = [...array, `I am ${i} strange loops.`];
+      //array.push(`I am ${i} strange loops.`);
       return array;
     }
   }
@@ -13,20 +15,17 @@ function forLoop(array) {
 function whileLoop(n) {
   while (n > 0) {
     console.log(--countdown);
-  } else if (n == 0){
-    console.log('done');
   }
+  console.log('done');
 }
 
 function doWhileLoop(array) {
   function maybeTrue() {
-    return Math.random() >= 0.5
+    return Math.random() >= 0.5;
   }
 
   do {
     array.slice(0, array.length - 1);
-  } while (array.length > 0 && (maybeTrue() == true);
-
+  } while (array.length > 0 && maybeTrue());
   return false;
-
 }
